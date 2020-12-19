c2bfda7897aa3f09158f0f883bc975072166c8b1
jhersonharyson <jherson_k-f@hotmail.com>
2020-12-19 03:59:16 -0300

    removed styled icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import bootstrap from "bootstrap";
+
 import "./style.css";
 import Routes from "./routes";
 const browserHistory = createBrowserHistory();
